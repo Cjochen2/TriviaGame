1d940008b6cc9fec4d8a9c5a4384d56363a08fbd
Curtis Jochen <curtis.jochen@gmail.com>
2019-06-17 19:01:48 -0400

    Added a whole mess of code to generate answers onto the page with the ability to select answers and only one answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,135 +3,136 @@ var questionsArr = [
     {
         question: 'What have I got in my pocket?',
 
-        answers: {
-            a: 'Elf Bread',
+        answers: [
+            'Elf Bread',
 
-            b: 'A Pen',
+            'A Pen',
 
-            c: 'The Ring',
-        },
+            'The Ring',
+        ],
         correctAnswer: 'c'
     },
 
     {
         question: 'What has roots as nobody sees, Is taller than trees, Up, up it goes, And yet never grows?',
 
-        answers: {
-            a: 'The Sky',
+        answers: [
+            'The Sky',
 
-            b: 'A Mountain',
+            'A Mountain',
 
-            c: 'A Skyscraper',
-        },
+            'A Skyscraper',
+        ],
         correctAnswer: 'b'
     },
 
     {
         question: 'Thirty white horses on a red hill, First they champ, Then they stamp, Then they stand still?',
 
-        answers: {
-            a: 'A Calvary Charge',
+        answers: [
+            'A Calvary Charge',
 
-            b: 'Teeth',
+            'Teeth',
 
-            c: 'Bones',
-        },
+            'Bones',
+        ],
         correctAnswer: 'b'
     },
 
     {
         question: 'Voiceless it cries, Wingless flutters, Toothless bites, Mouthless mutters?',
 
-        answers: {
-            a: 'The Wind',
+        answers: [
+            'The Wind',
 
-            b: 'A Ghost',
+            'A Ghost',
 
-            c: 'An Echo',
-        },
+            'An Echo',
+        ],
         correctAnswer: 'a'
     },
 
     {
         question: 'An eye in a blue face Saw an eye in a green face. "That eye is like to this eye" Said the first eye, "But in low place, Not in high place."?',
 
-        answers: {
-            a: 'A Cyclops',
+        answers: [
+            'A Cyclops',
 
-            b: 'The Earth',
+            'The Earth',
 
-            c: 'The Sun Shining on a Daisy',
-        },
+            'The Sun Shining on a Daisy',
+        ],
         correctAnswer: 'c'
     },
 
     {
         question: 'It cannot be seen, cannot be felt, Cannot be heard, cannot be smelt. It lies behind stars and under hills, And empty holes it fills. It comes first and follows after, Ends life, kills laughter?',
 
-        answers: {
-            a: 'Gas',
+        answers: [
+            'Gas',
 
-            b: 'Darkness',
+            'Darkness',
 
-            c: 'Time',
-        },
+            'Time',
+        ],
         correctAnswer: 'b'
     },
 
     {
         question: 'A box without hinges, key, or lid, Yet golden treasure inside is hid?',
 
-        answers: {
-            a: 'An Egg',
+        answers: [
+            'An Egg',
 
-            b: 'A Heart with Love',
+            'A Heart with Love',
 
-            c: 'A Person',
-        },
+            'A Person',
+        ],
         correctAnswer: 'a'
     },
 
     {
         question: 'Alive without breath, as cold as death, never thirsty, ever drinking, all in mail never clinking?',
 
-        answers: {
-            a: 'A Suit of Armor',
+        answers: [
+            'A Suit of Armor',
 
-            b: 'A Zombie',
+            'A Zombie',
 
-            c: 'A Fish',
-        },
+            'A Fish',
+        ],
         correctAnswer: 'c'
     },
 
     {
         question: 'No-legs lay on one-leg, two legs sat near on three legs, four legs got some?',
 
-        answers: {
-            a: 'Fish on a Table, Man on a Stool, Cat gets the scraps',
+        answers: [
+            'Fish on a Table, Man on a Stool, Cat gets the scraps',
 
-            b: 'Baby on a Table, Toddler on a lap, Old Man on a Walker',
+            'Baby on a Table, Toddler on a lap, Old Man on a Walker',
 
-            c: 'Sack of Seed on a Table, Kid on a Tricycle, Bird in a Cage on a Table',
-        },
+            'Sack of Seed on a Table, Kid on a Tricycle, Bird in a Cage on a Table',
+        ],
         correctAnswer: 'a'
     },
 
     {
         question: 'This thing all things devours: birds, beasts, trees, flowers; gnaws iron, bites steel; grinds hard stones to meal; slays king, ruins town, and beats high mountain down?',
 
-        answers: {
-            a: 'Time',
+        answers: [
+            'Time',
 
-            b: 'Fire',
+            'Fire',
 
-            c: 'Wind',
-        },
+            'Wind',
+        ],
         correctAnswer: 'a'
     },
 
 ];
 var questionGenerator = [];
+var answers = [];
 var score = 0;
 
 quizStart()
@@ -139,8 +140,7 @@ quizStart()
 function quizStart() {
 
 
-    var answers = [];
-
+    
     // Picks 5 questions out of 9 and logs them to a var to hold for further use
     while (questionGenerator.length < 5) {
 
@@ -155,12 +155,36 @@ function quizStart() {
     questionGenerator.push(questionsArr[0]);
     console.log(questionGenerator);
 
+
     // Prints the question to the HTML
     for (var i = 0; i < questionGenerator.length; i++) {
+        
         var k = i + 1;
+        
         var quesMaker = $('<div class = "quiz"> ');
         var ques = $('<p>').text('Question ' + k + ': ' + questionGenerator[i].question);
         quesMaker.append(ques);
+
+        var list = $('<ol type="a">')
+        
+        // Prints answers in a list with 
+        for(var j = 0; j < 3; j++){
+                
+            var li = $('<li>');
+            var label = $('<label>');
+            var input = $('<input type="radio">').attr({name: 'gues' + i, value: j});
+            
+            label.text(questionGenerator[i].answers[j])
+
+            label.prepend(input);
+
+            li.append(label);
+
+            list.append(li);
+        }
+        
+        $(quesMaker).append(list);
+       
 
         $("#questions").append(quesMaker);
         console.log(questionGenerator[i].question)
